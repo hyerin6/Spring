727e7211cdd4727d5b15fea8891db02bcbd2213b
hyerin6 <hyerinn6@gmail.com>
2019-05-07 21:57:37 +0900

    정적파일 추가

--- hello/src/main/java/net/skhu/HomeController.java
+++ hello/src/main/java/net/skhu/HomeController.java
@@ -3,6 +3,7 @@ package net.skhu;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Date;
 
 @Controller
 public class HomeController {
@@ -10,6 +11,7 @@ public class HomeController {
     @RequestMapping("/")
     public String index(Model model) { // view 에게 전달될 데이터를 변수 model에 전달
         model.addAttribute("message", "좋은 아침");
+        model.addAttribute("now", new Date());
         return "index"; 
     }
 }
