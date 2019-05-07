575940ae42d0f60587c263628c84ace7c7c40eeb
hyerin6 <hyerinn6@gmail.com>
2019-05-07 16:57:09 +0900

    hello

---
+++ hello/src/main/java/net/skhu/HomeController.java
@@ -0,0 +1,15 @@
+package net.skhu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) { // view 에게 전달될 데이터를 변수 model에 전달
+        model.addAttribute("message", "좋은 아침");
+        return "index"; 
+    }
+}
