d888cf9a486c9a4d92640cb984c4ddb621553764
hyerin6 <hyerinn6@gmail.com>
2019-10-02 00:37:04 +0900

    resultMap, association

--- mybatis/mybatis2/src/main/java/net/skhu/controller/MybatisController.java
+++ mybatis/mybatis2/src/main/java/net/skhu/controller/MybatisController.java
@@ -2,14 +2,12 @@ package net.skhu.controller;
 
 import java.util.List;
 
-
 import net.skhu.dto.Department;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 
 import net.skhu.dto.Student;
 import net.skhu.mapper.DepartmentMapper;
