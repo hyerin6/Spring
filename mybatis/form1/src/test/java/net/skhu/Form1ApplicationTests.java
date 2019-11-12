6d522aee899b304734ee7554c0dd0cec49e57a3f
hyerin6 <hyerinn6@gmail.com>
2019-11-12 16:27:30 +0900

    form01

---
+++ mybatis/form1/src/test/java/net/skhu/Form1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Form1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
